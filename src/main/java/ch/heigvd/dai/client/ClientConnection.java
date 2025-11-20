49b8fe4b3700b38dd6b0fcfa66ff8801138390db
Santiago Sugrañes <santiago.sugranes@heig-vd.ch>
2025-11-20 08:36:33 +0100

    fixed template

--- src/main/java/ch/heigvd/dai/client/ClientConnection.java
+++ src/main/java/ch/heigvd/dai/client/ClientConnection.java
@@ -18,6 +18,6 @@ class ClientConnection {
 
 	public String receive() {
 		// TODO
-		return "temp message";
+		return null;
 	}
 }
--- src/main/java/ch/heigvd/dai/client/ClientRunner.java
+++ src/main/java/ch/heigvd/dai/client/ClientRunner.java
@@ -5,18 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClientRunner {
-	private String host;
-	private int port;
 	private ClientUI ui;
 	private List<String> messages;
 	private List<String> channels;
+	private ClientConnection connection;
 
 	// TODO: handle ioexception here?
 	public ClientRunner(String host, int port) throws IOException {
-		this.host = host;
-		this.port = port;
+		this.ui = new ClientUI();
 		this.messages = new ArrayList<String>();
 		this.channels = List.of("channel1", "channel2");
+		this.connection = new ClientConnection(host, port);
 	}
 
 	/**
@@ -24,12 +23,19 @@ public class ClientRunner {
 	 * starting the REPL.
 	 */
 	public void init() {	
+		connection.connect();
+
 		while (true) {
 			// TODO: send and recieve messages
+			String msg;
+			while((msg = connection.receive()) != null) {
+				messages.add(msg);
+			}
 			ui.render(channels, messages);
 			// TODO: do we really want this in here?
 			String input = ui.readInput();
 			if (input.equals("/exit")) break;
+			connection.send(input);
 			messages.add(input);
 			if (messages.size() > 200) messages.remove(0);
 		}
